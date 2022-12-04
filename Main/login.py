06799fcd37695f1906ffc4e8f8949bb98632a309
GBLY327 <kimgunmin77@gmail.com>
2022-12-05 02:46:39 +0900

    add : login 창 및 thme 변경 버튼

---
+++ Main/login.py
@@ -0,0 +1,55 @@
+import customtkinter
+import tkinter
+
+appearance_mode = "dark"
+
+customtkinter.set_appearance_mode(appearance_mode)
+customtkinter.set_default_color_theme("green")
+
+root = customtkinter.CTk()
+root.geometry("500x400")
+
+def theme():
+    global appearance_mode
+
+    if appearance_mode == "dark":
+        customtkinter.set_appearance_mode("light")
+        appearance_mode = "light"
+    else:
+        customtkinter.set_appearance_mode("dark")
+        appearance_mode = "dark"
+
+def login():
+    print("Login")
+    print(f"ID : {entry1.get()}")
+    print(f"PW : {entry2.get()}")
+
+def signup():
+    print("Sign Up")
+
+frame = customtkinter.CTkFrame(master=root, width=500, height=350)
+frame.pack(padx=60, pady=20, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login", font=("Arial", 20, "bold"))
+label.pack(padx=10, pady=12)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="ID", takefocus=False)
+entry1.pack(padx=10, pady=5)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(padx=10, pady=5)
+entry2.bind("<Return>", command=login)
+
+login_button = customtkinter.CTkButton(master=frame, text="Login", command=login)
+login_button.pack(padx=10, pady=5)
+
+signup_button = customtkinter.CTkButton(master=frame, text="Sign Up", command=signup)
+signup_button.pack(padx=10, pady=5)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember ME")
+checkbox.pack(padx=10, pady=20)
+
+theme_button = customtkinter.CTkButton(master=frame, text="Theme", command=theme)
+theme_button.pack(padx=10, pady=5)
+
+root.mainloop()
