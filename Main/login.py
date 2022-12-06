265ba02f324e4d1b61cdcfd7ed9b8ed219ac3324
GBLY327 <kimgunmin77@gmail.com>
2022-12-06 16:34:05 +0900

    Update : class 생성

--- Main/login.py
+++ Main/login.py
@@ -2,80 +2,88 @@ import customtkinter
 import tkinter
 import time
 
-#appearance_mode 초기화
-appearance_mode = "dark"
+class App:
+    def __init__(self, master) -> None:
+        self.master = master
 
-customtkinter.set_appearance_mode(appearance_mode)
-customtkinter.set_default_color_theme("green")
+        # frame 설정
+        self.frame = customtkinter.CTkFrame(master=self.master, width=500, height=350)
+        self.frame.pack(padx=60, pady=20, fill="both", expand=True)
 
-root = customtkinter.CTk()
-root.geometry("500x400")
+        # label
+        self.label = customtkinter.CTkLabel(master=self.frame, text="Login", font=("Arial", 20, "bold"))
+        self.label.pack(padx=10, pady=12)
 
-#theme 함수 : appearance_mode에 따라 테마 변경
-def theme():
-    global appearance_mode
+        # ID Entry : ID 입력, Tab 키로 PW로 전환 가능
+        self.entry1 = customtkinter.CTkEntry(master=self.frame, placeholder_text="ID", takefocus=False)
+        self.entry1.pack(padx=10, pady=5)
 
-    if appearance_mode == "dark":
-        customtkinter.set_appearance_mode("light")
-        appearance_mode = "light"
-    else:
-        customtkinter.set_appearance_mode("dark")
-        appearance_mode = "dark"
+        # PW Entry : PW 입력, Enter 키로 login 함수 호출 가능
+        self.entry2 = customtkinter.CTkEntry(master=self.frame, placeholder_text="Password", show="*")
+        self.entry2.pack(padx=10, pady=5)
+        self.entry2.bind("<Return>", command=self.enter_to_login)
 
-#login 함수 : ID, PW가 입력되었는지 확인하고 ID, PW 출력
-def login():
-    print("Login")
-    if (entry1.get() == ''):
-        print("Please Write ID")
-    elif (entry2.get() == ''):
-        print("Please Wirte Password")
-    else:
-        print(f"ID : {entry1.get()}")
-        print(f"PW : {entry2.get()}")
+        # Login Button : login 함수 호출
+        self.login_button = customtkinter.CTkButton(master=self.frame, text="Login", command=self.login, state="normal")
+        self.login_button.pack(padx=10, pady=5)
 
-#signup 함수
-def signup():
-    print("Sign Up")
+        # Sign Up Button : signup 함수 호출
+        self.signup_button = customtkinter.CTkButton(master=self.frame, text="Sign Up", command=self.signup)
+        self.signup_button.pack(padx=10, pady=5)
 
-#enter_to_login 함수
-def enter_to_login(value):
-    login()
+        # Remember Me CheckBox
+        self.checkbox = customtkinter.CTkCheckBox(master=self.frame, text="Remember ME")
+        self.checkbox.pack(padx=10, pady=20)
 
-#frame 설정
-frame = customtkinter.CTkFrame(master=root, width=500, height=350)
-frame.pack(padx=60, pady=20, fill="both", expand=True)
+        # Theme Button : 테마 변경
+        self.theme_button = customtkinter.CTkButton(master=self.frame, text="Theme", command=self.theme, width=100, height=25)
+        self.theme_button.pack(padx=10, pady=5)
 
-#label
-label = customtkinter.CTkLabel(master=frame, text="Login", font=("Arial", 20, "bold"))
-label.pack(padx=10, pady=12)
+        # Exit Button : 종료
+        self.exit_button = customtkinter.CTkButton(master=self.frame, text="Exit", command=root.destroy, width=100, height=25)
+        self.exit_button.pack(padx=10, pady=5)
 
-#ID Entry : ID 입력, Tab 키로 PW로 전환 가능
-entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="ID", takefocus=False)
-entry1.pack(padx=10, pady=5)
+    # theme 함수 : appearance_mode에 따라 테마 변경
+    def theme(self) -> None:
+        global appearance_mode
 
-#PW Entry : PW 입력, Enter 키로 login 함수 호출 가능
-entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
-entry2.pack(padx=10, pady=5)
-entry2.bind("<Return>", command=enter_to_login)
+        if appearance_mode == "dark":
+            customtkinter.set_appearance_mode("light")
+            appearance_mode = "light"
+        else:
+            customtkinter.set_appearance_mode("dark")
+            appearance_mode = "dark"
 
-#Login Button : login 함수 호출
-login_button = customtkinter.CTkButton(master=frame, text="Login", command=login, state="normal")
-login_button.pack(padx=10, pady=5)
+    # login 함수 : ID, PW가 입력되었는지 확인하고 ID, PW 출력
+    def login(self) -> None:
+        print("Login")
 
-#Sign Up Button : signup 함수 호출
-signup_button = customtkinter.CTkButton(master=frame, text="Sign Up", command=signup)
-signup_button.pack(padx=10, pady=5)
+        if (self.entry1.get() == ''):
+            print("Please Write ID")
+        elif (self.entry2.get() == ''):
+            print("Please Wirte Password")
+        else:
+            print(f"ID : {self.entry1.get()}")
+            print(f"PW : {self.entry2.get()}")
 
-#Remember Me CheckBox
-checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember ME")
-checkbox.pack(padx=10, pady=20)
+    # signup 함수
+    def signup(self) -> None:
+        print("Sign Up")
 
-#Theme Button : 테마 변경
-theme_button = customtkinter.CTkButton(master=frame, text="Theme", command=theme, width=100, height=25)
-theme_button.pack(padx=10, pady=5)
+    # enter_to_login 함수
+    def enter_to_login(self, value) -> None:
+        self.login()
 
-#Exit Button : 종료
-exit_button = customtkinter.CTkButton(master=frame, text="Exit", command=root.destroy, width=100, height=25)
-exit_button.pack(padx=10, pady=5)
+if __name__ == "__main__":
+    #appearance_mode 초기화
+    appearance_mode = "dark"
 
-root.mainloop()
+    customtkinter.set_appearance_mode(appearance_mode)
+    customtkinter.set_default_color_theme("green")
+
+    root = customtkinter.CTk()
+    root.geometry("500x400")
+
+    app = App(root)
+
+    root.mainloop()
