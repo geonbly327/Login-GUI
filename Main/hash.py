4a70330535666cbb77eb0eefead3d21bbeace217
GBLY327 <kimgunmin77@gmail.com>
2022-12-08 02:08:01 +0900

    Update : 주석 추가

--- Main/hash.py
+++ Main/hash.py
@@ -2,6 +2,7 @@ import hashlib
 import random
 import string
 
+#hash_password 함수 : password를 salt와 함께 해싱하여 반환
 def hash_password(password) -> tuple(hashlib.sha256, str):
     salt = _salt()
     password = password + salt
@@ -10,12 +11,14 @@ def hash_password(password) -> tuple(hashlib.sha256, str):
 
     return hashed_password, salt
 
+#_salt 함수 : salt를 생성하여 반환
 def _salt() -> str:
     count = random.randint(16, 21)
     string_store = string.ascii_letters + string.digits + string.punctuation
     salt = "".join(random.choices(string_store, k=count))
     return salt
 
+#verify_password 함수 : password와 salt를 이용하여 user_password와 비교하여 True/False 반환
 def verify_password(password, user_password, salt) -> bool:
     password = password + salt
     hashed_password = hashlib.sha256(password.encode())
