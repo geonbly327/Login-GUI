75cd4063603f814f02e3e01ea7d2ac38d07b225b
GBLY327 <kimgunmin77@gmail.com>
2022-12-09 21:49:55 +0900

    .gitignore test

--- Main/mysql_user_info.py
+++ Main/mysql_user_info.py
@@ -1,1 +1,1 @@
-user_info = {'db' : 'abc', 'host' : '127.0.0.1', 'user' : 'root', 'passwd' : '', 'port' : 3306, 'charset' : 'utf8'}
+user_info = {'db' : 'abcd', 'host' : '127.0.0.1', 'user' : 'root', 'passwd' : '', 'port' : 3306, 'charset' : 'utf8'}
