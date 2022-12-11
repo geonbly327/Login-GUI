2c88c1a2dfc8cc5f4d7c058eb0415def0f088c73
GBLY327 <kimgunmin77@gmail.com>
2022-12-12 01:18:24 +0900

    Add : insert 함수 추가

--- Main/connect_db.py
+++ Main/connect_db.py
@@ -1,17 +1,25 @@
 import pymysql
+from pymysql import cursors
 import mysql_user_info
 
 _user = mysql_user_info.user_info
 
 def fetch():
     with pymysql.connect(db=_user["db"], host=_user["host"], user=_user["user"], passwd=_user["passwd"], port=_user["port"], charset=_user["charset"]) as db:
-        with db.cursor(pymysql.cursors.DictCursor) as cur:
+        with db.cursor(cursors.DictCursor) as cur:
             sql = "SELECT * FROM user"
             cur.execute(sql)
             db.commit()
             data = cur.fetchall()
 
     return data
+
+def insert(id, pw, salt):
+    with pymysql.connect(db=_user["db"], host=_user["host"], user=_user["user"], passwd=_user["passwd"], port=_user["port"], charset=_user["charset"]) as db:
+        with db.cursor(cursors.DictCursor) as cur:
+            sql = "INSERT INTO user (id, pw, salt) VALUES (%s, %s, %s)"
+            cur.execute(sql, (id, pw, salt))
+            db.commit()
 
 if __name__ == "__main__":
     data = fetch()
