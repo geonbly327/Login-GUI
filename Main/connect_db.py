d82a9b07bd956be0771b88a07fa34c544b7eea6a
GBLY327 <kimgunmin77@gmail.com>
2022-12-13 15:28:16 +0900

    Delete : fetch 함수 필요성을 못느껴 삭제

--- Main/connect_db.py
+++ Main/connect_db.py
@@ -4,16 +4,6 @@ import mysql_user_info
 
 _user = mysql_user_info.user_info
 
-def fetch():
-    with pymysql.connect(db=_user["db"], host=_user["host"], user=_user["user"], passwd=_user["passwd"], port=_user["port"], charset=_user["charset"]) as db:
-        with db.cursor(cursors.DictCursor) as cur:
-            sql = "SELECT * FROM user"
-            cur.execute(sql)
-            db.commit()
-            data = cur.fetchall()
-
-    return data
-
 def insert(id, pw, salt):
     with pymysql.connect(db=_user["db"], host=_user["host"], user=_user["user"], passwd=_user["passwd"], port=_user["port"], charset=_user["charset"]) as db:
         with db.cursor(cursors.DictCursor) as cur:
