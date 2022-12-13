571d7412846d7024189639948089505dc2f6392c
GBLY327 <kimgunmin77@gmail.com>
2022-12-13 14:19:55 +0900

    Update : _salt 함수를 create_salt 로 변환

--- Main/hash.py
+++ Main/hash.py
@@ -4,15 +4,15 @@ import string
 
 #hash_password 함수 : password를 salt와 함께 해싱하여 반환
 def hash_password(password) -> tuple([hashlib.sha256, str]):
-    salt = _salt()
+    salt = create_salt()
     password = password + salt
 
     hashed_password = hashlib.sha256(password.encode())
 
     return hashed_password, salt
 
-#_salt 함수 : salt를 생성하여 반환
-def _salt() -> str:
+#create_salt 함수 : salt를 생성하여 반환
+def create_salt() -> str:
     count = random.randint(16, 21)
     string_store = string.ascii_letters + string.digits + string.punctuation
     salt = "".join(random.choices(string_store, k=count))
